8794f0ab17dc3b8caa91fecc79880a04e0e229a7
jmjmjames <chungjm0711@gmail.com>
2022-12-09 13:33:01 +0900

    #15 test: spring Transactional Self invocation Test

--- src/main/java/com/example/recommendationservice/domain/pharmacy/service/PharmacyService.java
+++ src/main/java/com/example/recommendationservice/domain/pharmacy/service/PharmacyService.java
@@ -2,19 +2,37 @@ package com.example.recommendationservice.domain.pharmacy.service;
 
 import com.example.recommendationservice.domain.pharmacy.entity.Pharmacy;
 import com.example.recommendationservice.domain.pharmacy.repository.PharmacyRepository;
+import java.util.List;
 import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 
 @Slf4j
 @Service
-@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class PharmacyService {
 
     private final PharmacyRepository pharmacyRepository;
+
+    // self invocation test
+    public void bar(List<Pharmacy> pharmacyList) {
+        log.info("bar CurrentTransactionName: " + TransactionSynchronizationManager.getCurrentTransactionName());
+        foo(pharmacyList);
+    }
+
+    // self invocation test
+    @Transactional
+    public void foo(List<Pharmacy> pharmacyList) {
+        log.info("foo CurrentTransactionName: " + TransactionSynchronizationManager.getCurrentTransactionName());
+        pharmacyList.forEach(pharmacy -> {
+            pharmacyRepository.save(pharmacy);
+            throw new RuntimeException("throw error");
+        });
+    }
+
 
     @Transactional
     public void updateAddress(Long id, String address) {
--- src/test/groovy/com/example/recommendationservice/domain/pharmacy/service/PharmacyServiceTest.groovy
+++ src/test/groovy/com/example/recommendationservice/domain/pharmacy/service/PharmacyServiceTest.groovy
@@ -78,4 +78,27 @@ class PharmacyServiceTest extends AbstractIntegrationContainerBaseTest {
         then:
         result.get(0).getPharmacyAddress() == address
     }
+
+    def "self invocation"() {
+        given:
+        String address = "서울특별시 성북구 보문동"
+        String name = "가까운 약국"
+        double latitude = 37.58
+        double longitude = 127.02
+
+        def pharmacy = Pharmacy.builder()
+                .pharmacyAddress(address)
+                .pharmacyName(name)
+                .latitude(latitude)
+                .longitude(longitude)
+                .build()
+
+        when:
+        pharmacyService.bar(Arrays.asList(pharmacy))
+
+        then:
+        def e = thrown(RuntimeException.class)
+        def result = pharmacyRepository.findAll()
+        result.size() == 1  // 트랜잭션이 적용되지 않는다. (롤백이 안됨)
+    }
 }
